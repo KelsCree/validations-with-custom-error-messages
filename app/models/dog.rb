283e1e92899041e3bd72e198251543852607b5c7
KelsCree <kelseycreehan@gmail.com>
2020-09-16 06:45:46 -0600

    custom error messages

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,12 @@
 class Dog < ApplicationRecord
+  validates :name, presence: { message: "Give me a name!" }, uniqueness: { case_sensitive: false, message: "Don't be a copycat"}
+
+  # custom validation
+  
+  validate :name, :cannot_be_vulgar
+  def cannot_be_vulgar
+    if name.downcase == 'shit'
+      errors.add(:name, 'clean that shit up')
+    end
+  end
 end
